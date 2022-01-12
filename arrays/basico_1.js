4e20112908e062177c8d662ab7a493a7bf3b39aa
Samuel <samuelbalanibergara@yahoo.com.br>
2022-01-12 00:42:37 -0300

    Feito por completo o curso!

---
+++ arrays/basico_1.js
@@ -0,0 +1,11 @@
+//              0    1    2    3   4    5
+const notas = [5.6, 2.6, 1.7, 6.5, 10, 8.2];
+
+console.log(notas);
+console.log(notas[3]);
+console.log(notas[5]);
+console.log(notas[10]);
+
+notas[4] = 9.8;
+
+console.log(notas);
